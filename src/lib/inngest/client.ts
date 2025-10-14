0fc344f096aa50a38651d634769372304d98b10e
Bharat Chhabra <bchhabra2490@gmail.com>
2025-10-14 20:43:14 +0530

    Updated inngest client initiation

--- src/lib/inngest/client.ts
+++ src/lib/inngest/client.ts
@@ -3,6 +3,7 @@ import { Inngest } from 'inngest';
 export const inngest = new Inngest({
   id: 'personal-anything-notifier',
   name: 'Personal Anything Notifier',
+  eventKey: process.env.INNGEST_EVENT_KEY,
 });
 
 
